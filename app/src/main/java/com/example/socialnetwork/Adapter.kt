e65330b375865c77e8ae9bfa5d4ca889b2dc633c
ArtSav23 <savvaartem2301@gmail.com>
2022-07-13 16:01:05 +0300

    amendments

--- app/src/main/java/com/example/socialnetwork/Adapter.kt
+++ app/src/main/java/com/example/socialnetwork/Adapter.kt
@@ -1,16 +1,17 @@
 package com.example.socialnetwork
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.socialnetwork.databinding.ViewBinding
 
 class Adapter(private val listener: Listener): RecyclerView.Adapter<ViewHolder>() {
 
-    private var publicationModel = mutableListOf(PublicationModel(R.drawable.ic_add, "1"),
-        PublicationModel( R.drawable.ic_message, "2"),PublicationModel(com.google.android.material.R.drawable.abc_btn_check_material, "3"),
-        PublicationModel(R.drawable.ic_launcher_foreground, "4"),PublicationModel(R.drawable.ic_baseline_home_24, "5"),)
+    private var publicationModels = mutableListOf (
+        PublicationModel(R.drawable.ic_add, "1"),
+        PublicationModel( R.drawable.ic_message, "2"),
+        PublicationModel(com.google.android.material.R.drawable.abc_btn_check_material, "3"),
+        PublicationModel(R.drawable.ic_launcher_foreground, "4"),
+        PublicationModel(R.drawable.ic_baseline_home_24, "5"))
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view=LayoutInflater.from(parent.context).inflate(R.layout.view, parent, false)
@@ -18,19 +19,19 @@ class Adapter(private val listener: Listener): RecyclerView.Adapter<ViewHolder>(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(publicationModel[position], listener)
+        holder.bind(publicationModels[position], listener)
     }
 
     override fun getItemCount(): Int {
-        return publicationModel.size
+        return publicationModels.count()
     }
 
-    fun add (data: PublicationModel){
-        publicationModel.add(data)
-        notifyDataSetChanged()
-    }
+//    fun add (data: PublicationModel) {
+//        publicationModels.add(data)
+//        notifyDataSetChanged()
+//    }
 
-    interface Listener{
+    interface Listener {
         fun onClick(data: PublicationModel)
     }
 }
